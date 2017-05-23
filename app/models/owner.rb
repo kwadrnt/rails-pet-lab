4f9c8dcdd18e0c7c191179ff8fc5408ce8eaa970
kwadrnt <kevinctse@gmail.com>
2017-05-23 11:59:08 -0700

    passed all rspec tests for part 5, validations work for new form

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,15 +1,25 @@
 class Owner < ActiveRecord::Base
-  has_many :pets
+  # TODO: add association ot pets
+  has_many :pets, dependent: :destroy
 
   # TODO: add association to appointments (through pets)
 
   # TODO: add validations
+  validates :first_name, presence: true, uniqueness: true, length: {maximum: 255}
+  validates :last_name, presence: true, uniqueness: true, length: {maximum: 255}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true, length: {maximum: 255}
+  validates :phone, presence: true, uniqueness: true, length: {maximum: 255}
 
   before_save :normalize_phone_number
 
   # removes leading 1 and the characters (, ), -, .
   def normalize_phone_number
     # stretch
+    if phone.present?
+      phone.gsub!(/^1/, "")
+      phone.gsub!(/[()-.]/,"")
+    end
   end
 
 end
